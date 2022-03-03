c29e851485a0d7234f25f87cefdd7fafdce03096
MislavSag <mislav.sagovac@contentio.biz>
2022-03-03 13:24:19 +0100

    change package imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,16 @@
 #' @import R6
 #' @import data.table
 #' @import checkmate
-#' @import httr
 #' @import AzureStor
 #' @import RcppQuantuccia
 #' @import rvest
 #' @import stringr
 #' @import AzureStor
-#' @import readr
 #' @import QuantTools
 #' @import pins
 #' @import stringr
 #' @importFrom jsonlite fromJSON
+#' @importFrom httr GET POST RETRY
 #' @importFrom zip zipr
 "_PACKAGE"
 
