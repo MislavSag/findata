49de714771eae7c3a7ab00d36a12cd0fabf67b67
MislavSag <mislav.sagovac@contentio.biz>
2022-03-04 14:32:14 +0100

    bug in zzz

--- R/zzz.R
+++ R/zzz.R
@@ -10,7 +10,7 @@
 #' @import pins
 #' @import stringr
 #' @importFrom jsonlite fromJSON
-#' @importFrom httr GET POST RETRY
+#' @importFrom httr GET POST RETRY content
 #' @importFrom zip zipr
 "_PACKAGE"
 
