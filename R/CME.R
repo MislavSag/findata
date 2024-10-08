effc4076122d711062d1ef4ce192cd60ebd06f4d
MislavSag <mislav.sagovac@contentio.biz>
2024-10-08 15:47:56 +0200

    CME class

--- R/CME.R
+++ R/CME.R
@@ -67,13 +67,15 @@ CME = R6::R6Class(
         encode = "form"
       )
       token_l = content(auth_response)
-      return(token_l$access_token)
+      token = token_l$access_token
+      attr(token, "expires") = Sys.time() + 29 * 60
+      return(token)
     },
     
     #' @description
     #' Refresh the authentication token if it's expired or missing.
     retoken = function() {
-      if (is.null(self$token)) {
+      if (is.null(self$token) || attr(self$token, "expires") < Sys.time()) {
         self$token = self$get_token()
       }
     },
@@ -84,19 +86,26 @@ CME = R6::R6Class(
     #' @param meeting_date Optional. Filter forecasts by specific meeting date in 'yyyy-mm-dd' format.
     #' @param reporting_date Optional. Filter forecasts by specific reporting date in 'yyyy-mm-dd' format.
     #' @param limit Optional. Limits the number of results returned. Default is 10.
+    #' @param remove_na_zero Optional. Remove rows with NA or zero probability. Default is TRUE.
     #'
     #' @return A list containing the FedWatch forecast data.
-    fedwatch = function(meeting_date = NULL, reporting_date = NULL, limit = 10) {
+    fedwatch = function(meeting_date = NULL, reporting_date = NULL, limit = 10,
+                        remove_na_zero = TRUE) {
       # Ensure the token is valid
-      # self$retoken()
-      
+      self$retoken()
+    
+      # Debug
+      # reporting_date = "2018-09-26"
+      # meeting_date  = "2018-08-01"
+      # limit = 99
+        
       # Prepare query parameters
       query_params = list(
         meetingDt = meeting_date,
         reportingDt = reporting_date,
         limit = limit
       )
-
+    
       # Make the GET request to the endpoint
       response = GET(
         url = "https://markets.api.cmegroup.com/fedwatch/v1/forecasts",
@@ -106,10 +115,21 @@ CME = R6::R6Class(
       
       # Check if the request was successful
       if (response$status_code == 200) {
-        return(content(response))
+        fw = content(response)
+        if (length(fw) == 0) {
+          return(NULL)
+        }
       } else {
         stop("Failed to retrieve FedWatch forecast. Status code: ", response$status_code)
       }
+      
+      # Clean
+      fw = rbindlist(fw)
+      fw = fw[, rbindlist(.SD[[1]]), by = .(meetingDt, reportingDt)]
+      if (remove_na_zero) {
+        fw = fw[!is.na(probability) & probability != 0]
+      }
+      return(fw)
     },
     
     #' @description
@@ -120,7 +140,7 @@ CME = R6::R6Class(
     #' @return A list containing the meetings history data.
     get_meetings_history = function(limit = 10) {
       # Ensure the token is valid
-      # self$retoken()
+      self$retoken()
       
       # Make the GET request to the endpoint
       response = GET(
@@ -131,10 +151,64 @@ CME = R6::R6Class(
       
       # Check if the request was successful
       if (response$status_code == 200) {
-        return(content(response))
+        mh = content(response)
       } else {
+        print(content(response))
         stop("Failed to retrieve meetings history. Status code: ", response$status_code)
       }
+      
+      # Clean data
+      mh = rbindlist(mh)
+      mh[, meetingDt  := as.Date(meetingDt)]
+      return(mh)
+    },
+    
+    #' @description
+    #' Get all history fedwatch data
+    #' 
+    #' @return data.table of lower and upper rates for mmeting dates and report dates
+    fedwatch_history = function() {
+      self$retoken()
+      
+      # Get all meeting dates
+      mh = self$get_meetings_history(limit = 100)
+      latest = self$fedwatch(limit = 100)[, .(meetingDt = as.Date(first(meetingDt)))]
+      mh = rbind(mh, latest, fill = TRUE)
+      setorder(mh, -meetingDt)
+      
+      # get fedwatch data for every meeting date
+      dates_ = mh[, meetingDt]
+      fw_l = lapply(seq_along(dates_), function(i) {
+        # i = 56
+        print(i)
+        
+        # Create date sequence
+        second_date = dates_[i]
+        if (length(dates_) > i) {
+          first_date  = dates_[i + 1]  
+        } else {
+          first_date = second_date - 56
+        }
+        
+        reports_dates = seq.Date(from = first_date, to = second_date-1, by = "days")
+        reports_dates = reports_dates[reports_dates < Sys.Date()]
+        
+        # Get fedwatch data
+        fw = lapply(reports_dates, function(x) {
+          print(x)
+          if (!isBusinessDay(x)) return(NULL)
+          tryCatch({self$fedwatch(meeting_date = second_date, reporting_date = x)}, 
+                   error = function(e) NULL)
+        })
+        rbindlist(fw)
+      })
+      fwdt = rbindlist(fw_l)
+      
+      # Clean data
+      fwdt[, let(meetingDt = as.Date(meetingDt ), 
+                 reportingDt = as.Date(reportingDt))]
+      
+      return(fwdt)
     }
   ),
   
